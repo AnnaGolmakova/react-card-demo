8cee44d62fab891fb70bfc4d126cef15312cda5c
Anna Golmakova <ann.golmakova@gmail.com>
2025-07-03 16:38:14 +0100

    Remove currency from locale context

--- src/context/LocaleContext.tsx
+++ src/context/LocaleContext.tsx
@@ -3,15 +3,11 @@ import { createContext, useContext, useState, type ReactNode } from "react";
 interface LocaleContextType {
   locale: string;
   setLocale: (locale: string) => void;
-  currency: string;
-  setCurrency: (currency: string) => void;
 }
 
 const LocaleContext = createContext<LocaleContextType>({
   locale: "ru-RU",
   setLocale: () => {},
-  currency: "RUB",
-  setCurrency: () => {},
 });
 
 interface LocaleProviderProps {
@@ -20,17 +16,16 @@ interface LocaleProviderProps {
 
 export function LocaleProvider({ children }: LocaleProviderProps) {
   const [locale, setLocale] = useState("ru-RU");
-  const [currency, setCurrency] = useState("RUB");
-
-  const value: LocaleContextType = {
-    locale,
-    setLocale,
-    currency,
-    setCurrency,
-  };
 
   return (
-    <LocaleContext.Provider value={value}>{children}</LocaleContext.Provider>
+    <LocaleContext.Provider
+      value={{
+        locale,
+        setLocale,
+      }}
+    >
+      {children}
+    </LocaleContext.Provider>
   );
 }
 
