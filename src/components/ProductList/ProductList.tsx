6609390440816fc8c2169493a989c43384530693
Anna Golmakova <ann.golmakova@gmail.com>
2025-07-05 13:50:39 +0100

    Add key to list

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -9,7 +9,7 @@ export const ProductList = (props: ProductListProps) => {
   return (
     <ul className="grid auto-rows-fr grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-4">
       {props.elements.map((element) => (
-        <li>
+        <li key={element.id}>
           <ProductCard className="h-full" {...element} />
         </li>
       ))}
